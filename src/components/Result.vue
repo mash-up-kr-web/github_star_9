dad4c12d51cfbfcbf141c00c3c99f9a40dfb1940
joi0104 <joi0104@naver.com>
2020-03-23 18:11:42 +0900

    Create search,result component in home

---
+++ src/components/Result.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="result">
+    result
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Result',
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="home">
-    home
+  <div id="home">
+    <search />
+    <result />
   </div>
 </template>
 
 <script>
+import Search from '../components/Search.vue';
+import Result from '../components/Result.vue';
+
 export default {
   name: 'Home',
   components: {
+    Search,
+    Result,
   },
 };
 </script>
