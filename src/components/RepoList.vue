bdd998ad2a8468e2fb5db2f0b2862ffc3c242cd0
joi0104 <joi0104@naver.com>
2020-03-25 00:59:35 +0900

    Add repoList if notEmpty and vaild userName

---
+++ src/components/RepoList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="repoList">
+    <h1>{{ userName }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RepoList',
+  computed: {
+    userName() {
+      return this.$store.state.userName;
+    },
+  },
+};
+</script>
--- src/components/Result.vue
+++ src/components/Result.vue
@@ -7,13 +7,24 @@
       <p v-if="!isVaildUserName()">
         일치하는 사용자가 존재하지 않습니다.
       </p>
+      <repo-list v-else />
     </div>
   </div>
 </template>
 
 <script>
+import RepoList from './RepoList.vue';
+
 export default {
   name: 'Result',
+  data() {
+    return {
+      vaildUserName: null,
+    };
+  },
+  components: {
+    RepoList,
+  },
   computed: {
     userName() {
       return this.$store.state.userName;
@@ -25,10 +36,24 @@ export default {
       return true;
     },
     isVaildUserName() {
+      this.fecthUserName();
+      return this.vaildUserName;
+    },
+    fecthUserName() {
       this.$axios.get(`https://api.github.com/users/${this.userName}`)
-        .then(() => true)
-        .catch(() => false);
+        .then(() => {
+          this.vaildUserName = true;
+        })
+        .catch(() => {
+          this.vaildUserName = false;
+        });
     },
   },
 };
 </script>
+
+<style scoped>
+#result {
+  margin-left: 30px;
+}
+</style>
