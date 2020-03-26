fed01f22cd3dfc985f7ccd976b8266836bc0c009
joi0104 <joi0104@naver.com>
2020-03-26 13:36:31 +0900

    Create repolist ui

--- src/components/RepoList.vue
+++ src/components/RepoList.vue
@@ -1,15 +1,48 @@
 <template>
   <div id="repoList">
     <h1>{{ userName }}</h1>
+    <p v-show="!isEmptyRepoList">
+      레포지토리가 존재하지 않습니다.
+    </p>
+    <p v-show="isEmptyRepoList">
+      {{ repoNameList }}
+    </p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'RepoList',
+  props: {
+    userName: String,
+  },
+  data() {
+    return {
+      isFetchSuccess: null,
+      repoNameList: [],
+    };
+  },
+  updated() {
+    this.fecthRepoList();
+  },
   computed: {
-    userName() {
-      return this.$store.state.userName;
+    isEmptyRepoList() {
+      return this.isFetchSuccess;
+    },
+  },
+  methods: {
+    fecthRepoList() {
+      this.$axios.get(`https://api.github.com/users/${this.userName}/repos`)
+        .then((res) => {
+          this.isFetchSuccess = true;
+          this.repoNameList = [];
+          res.data.forEach((element) => {
+            this.repoNameList.push(element.name);
+          });
+        })
+        .catch(() => {
+          this.isFetchSuccess = false;
+        });
     },
   },
 };
