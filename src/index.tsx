5fa3a3e650033004c9a6238d14decb2c0bbb8576
YukJiSoo <wltn3231@naver.com>
2020-03-31 01:39:08 +0900

    feat(style): add global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyle from './GlobalStyle';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById('root'),
+);
