a2f595ee162542fc4edfa95d415d9d906e2e192a
joi0104 <joi0104@naver.com>
2020-04-01 00:51:33 +0900

    Create RepoListItem

---
+++ src/components/RepoListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="repoListItem">
+    {{ repoName }}
+    {{ repoStar }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RepoListItem',
+  props: {
+    repoName: {
+      type: String,
+      default: '',
+    },
+    repoStar: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
