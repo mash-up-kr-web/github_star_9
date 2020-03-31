16fd361633c48a702d01ea21837a6b085ea52cf7
YukJiSoo <wltn3231@naver.com>
2020-03-31 19:43:40 +0900

    feat(style): add search page style

--- src/components/search/SearchPage.tsx
+++ src/components/search/SearchPage.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Button from '~/components/common/Button';
 import Input from '~/components/common/Input';
+
+const SearchPageLayout = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+
+  position: absolute;
+  top: 30%;
+
+  .title {
+    font-size: 3rem;
+  }
+
+  .sub-title {
+    font-size: 2rem;
+  }
+`;
 
 interface SearchPageProps {}
 
 const SearchPage: React.FC<SearchPageProps> = () => {
   return (
-    <main>
-      <h1>Gitstar Ranking</h1>
-      <h3>Unofficial GitHub star ranking for users, organizations and repositories.</h3>
+    <SearchPageLayout>
+      <h1 className="title">Gitstar Ranking</h1>
+      <h3 className="sub-title">Unofficial GitHub star ranking for users, organizations and repositories.</h3>
       <Input type="text" placeholder="Write user or organization name" />
       <Button>Search</Button>
-    </main>
+    </SearchPageLayout>
   );
 };
 
