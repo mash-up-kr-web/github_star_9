d61df21c1415cfab1c93912d3142845c27bba86e
lallaheeee <ooop0422@gmail.com>
2020-04-05 22:13:06 +0900

    [fix] 검색 API 요청 response status 404시 Not Found 추가

---
+++ src/components/atoms/NotFound/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.scss';
+
+const NotFound = () => (
+  <div className="not_found">
+    <p>
+      We&apos;re Sorry
+      <span role="img" aria-label="Chick">
+        🐣
+      </span>
+    </p>
+    <p>We can&apos;t Find it. please try again.</p>
+  </div>
+);
+
+export default NotFound;
--- src/components/organisms/GitStarRanking/index.jsx
+++ src/components/organisms/GitStarRanking/index.jsx
@@ -1,22 +1,35 @@
 import React, { useState } from 'react';
 import Spinner from '@atoms/Spinner';
+import NotFound from '@atoms/NotFound';
 import SearchBar from '@molecules/SearchBar';
 import SearchResult from '@organisms/SearchResult';
 
 import * as api from 'apis';
 
+const PageBody = ({ status: { isLoading, is404 }, ...rest }) => {
+  if (isLoading) return <Spinner />;
+  if (is404) return <NotFound />;
+
+  return <SearchResult {...rest} />;
+};
+
 export default function GitStarRanking() {
   const [keyword, setKeyword] = useState('');
   const [result, setResult] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [is404, setIs404] = useState(false);
 
   const onSearch = word => {
     setLoading(true);
+    setIs404(false);
     api
       .getRepos(word)
       .then(({ owner, repos }) => {
         setKeyword(owner);
         setResult(repos);
+      })
+      .catch(err => {
+        if (err.response.status === 404) setIs404(true);
       })
       .finally(() => setLoading(false));
   };
@@ -29,16 +42,13 @@ export default function GitStarRanking() {
         repositories.
       </p>
       <SearchBar onSearch={onSearch} />
-      {loading ? (
-        <Spinner />
-      ) : (
-        <SearchResult
-          name={keyword}
-          resultItems={result}
-          repoCount={result.length}
-          starCount={result.reduce((acc, cur) => acc + cur.starCount, 0)}
-        />
-      )}
+      <PageBody
+        status={{ isLoading, is404 }}
+        name={keyword}
+        resultItems={result}
+        repoCount={result.length}
+        starCount={result.reduce((acc, cur) => acc + cur.starCount, 0)}
+      />
     </>
   );
 }
