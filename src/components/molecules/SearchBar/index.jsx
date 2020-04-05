eaa710e0d9b7910e3c179f9133daaad5b23928d3
lallaheeee <ooop0422@gmail.com>
2020-04-05 22:22:10 +0900

    [fix] enter 검색이 가능하도록  검색창 수정
    더불어 클릭시 input 창의 value가 비워지도록 수정

--- src/components/molecules/SearchBar/index.jsx
+++ src/components/molecules/SearchBar/index.jsx
@@ -2,6 +2,8 @@ import React, { useRef } from 'react';
 import Button from '@atoms/Button';
 import './style.scss';
 
+const ENTER = 'Enter';
+
 export default function SearchBar({ onSearch }) {
   const searchInput = useRef(null);
 
@@ -10,11 +12,23 @@ export default function SearchBar({ onSearch }) {
     searchInput.current.focus();
   };
 
+  const handleKeyUp = ({ key }) => {
+    if (key !== ENTER) return;
+
+    handleOnSearch();
+  };
+
+  const handleOnClick = () => {
+    searchInput.current.value = '';
+  };
+
   return (
     <div className="search_bar">
       <input
         placeholder="github user 또는 organization을 검색해보세요."
         ref={searchInput}
+        onKeyUp={handleKeyUp}
+        onClick={handleOnClick}
       />
       <Button name="search" onClick={handleOnSearch} />
     </div>
