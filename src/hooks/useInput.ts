1d5d388680bc278fe30c12e6348ccbb965390e69
mango906 <minkb01@gmail.com>
2020-04-18 01:38:37 +0900

    refactor: useInput 간소화

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,25 +1,17 @@
-import { useReducer } from 'react';
+import { useState } from 'react';
 
 interface State {
   [key: string]: string;
 }
 
-interface Action {
-  name: string;
-  value: string;
-}
-
-function reducer(state: State, action: Action) {
-  return {
-    ...state,
-    [action.name]: action.value
-  };
-}
-
 export default function useInput(initialForm: State) {
-  const [state, dispatch] = useReducer(reducer, initialForm);
+  const [state, setState] = useState(initialForm);
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(e.target);
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    });
   };
 
   return { state, onChange };
