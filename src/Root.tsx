9c59b4c88530265b5f991cbea91d78033aa7a710
mango906 <minkb01@gmail.com>
2020-06-09 18:00:11 +0900

    chore: husky 테스트

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import Main from './containers/Main';
+import Main from "./containers/Main";
 
-const Root: React.FC = () => <Main />;
+const Root = () => <Main />;
 
 export default Root;
